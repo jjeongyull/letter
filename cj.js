1bb29abb0f218ca1ed07d885e44bff1c7bb1d833
jjenjgyull <ckddnjs3698@naver.com>
2023-12-22 22:12:29 +0900

    first commit

---
+++ cj.js
@@ -0,0 +1,13 @@
+$(document).on('click', '#btn', function(){
+  let name = $('#name').val();
+
+  if(name === "20231210"){
+    $('#div').empty();
+    $('#div').load('letter.html', () => {
+      AOS.init();
+    });
+  }else{
+    alert('틀렸다');
+    return;
+  }
+});
